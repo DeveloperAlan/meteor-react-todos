6d9442fceb6473bdb1913431a3f24fa3e12995e8
DeveloperAlan <alanng95@hotmail.com>
2016-08-15 08:08:03 +1000

    added imports

---
+++ imports/ui/App.jsx
@@ -0,0 +1,33 @@
+import React, { Compnent } from 'react';
+
+import Task from './Task.jsx';
+
+export default class App extends Compnent {
+	getTasks() {
+		return {
+			{_id: 1, text: "This is task 1"},
+			{_id: 2, text: "This is task 2"},
+			{_id: 3, text: "This is task 3"}
+		}
+	}
+
+	renderTasks() {
+		return this.getTasks().map((task) => (
+			<Task key={task._id} task={task} />
+		));
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<header>
+					<h1>Todo List</h1>
+				</header>
+
+				<ul>
+					{this.renderTasks()}
+				</ul>
+			</div>
+		);	
+	}
+}
